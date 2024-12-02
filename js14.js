262de20a6e4b946627768ed2cbfcb7f5b9192bf2
MuhammedMidlajk <midlajmuhamd@gmial.com>
2024-12-02 10:35:58 +0530

    QUESTION 14

---
+++ js14.js
@@ -0,0 +1,15 @@
+let time=18
+
+if(time>=0&&time<=11) {
+
+    console.log("morning")
+}
+else if(time>=12&&time<=17){
+
+    console.log("afternoon")
+}
+else if(time>=18&&time<=23){
+
+    console.log("evening")
+
+} 
