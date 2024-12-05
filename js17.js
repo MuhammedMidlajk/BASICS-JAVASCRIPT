b5c29c130c71ee81ea364d3ee5a28cc13d9b251f
MuhammedMidlajk <midlajmuhamd@gmial.com>
2024-12-05 10:29:07 +0530

    QUESTION 17

---
+++ js17.js
@@ -0,0 +1,36 @@
+let age=10;
+
+if(age>=1&&age<=10 ){
+
+    console.log("small")
+}
+else if(age>=11&&age<=50){
+
+    console.log("medium")
+}
+else if(age>=51){
+
+    console.log("large")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
