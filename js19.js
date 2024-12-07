31d1fbaa035eefff89ed500c5367dbb34ec0e421
MuhammedMidlajk <midlajmuhamd@gmial.com>
2024-12-07 10:28:18 +0530

    QUESTION 19

---
+++ js19.js
@@ -0,0 +1,17 @@
+let a="cloudy"
+
+if(a=="sunny"){
+
+    console.log("wear sunscreen")
+}
+else if(a=="rainy"){
+
+    console.log("bring an umbrella")
+}
+else if(a=="cloudy"){
+
+    console.log("Wear a light jacket")
+}
+else{
+    console.log("Check the forecast again")
+}
