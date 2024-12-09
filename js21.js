9d0aad5589c70f327191eafa6ca8dc5c31f6c1eb
MuhammedMidlajk <midlajmuhamd@gmial.com>
2024-12-09 10:02:16 +0530

    QUESTION 21

---
+++ js21.js
@@ -0,0 +1,18 @@
+let a="monday"
+
+if(a=="monday"){
+
+    console.log("Back to work")
+}
+else if(a=="friday"){
+
+    console.log("almost weekend!")
+}
+else if(a=="saturday"){
+
+    console.log("weekend")
+}
+else{
+
+    console.log("Just another day!")
+}
